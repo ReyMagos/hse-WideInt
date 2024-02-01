d4ae29b81016a0fa8d47240a34a216bab19adf2e
reymagos <andalepotapov@edu.hse.ru>
2024-02-02 02:32:54 +0300

    feature: unary minus overload, copy constructor
    Added unary minus overloading
    Added copy constructor
    Improved error message about unexpected character in constructor
    Removed TODO warning about several floating points as long as literal doesn't allow it

--- src/WideInt.cxx
+++ src/WideInt.cxx
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
+#include <format>
 
 #include "../include/WideInt.h"
 
@@ -30,7 +31,7 @@
 WideInt::WideInt(std::string num) {
     bool is_zero = true;
 
-    // Actually `start` and `end` shows index of the first and the last
+    // Actually `start` and `end` show index of the first and the last
     // non-zero digit in number respectively
     int start, end;
     int point = num.size();
@@ -47,10 +48,10 @@ WideInt::WideInt(std::string num) {
             }
             end = i;
         } else if (c == '.') {
-            // FIXME: Point will be overwritten if number has several of them
-             point = i;
+            point = i;
         } else if (c != '0') {
-            throw std::runtime_error("Unexpected symbol");
+            throw std::runtime_error(
+                    std::format("Unexpected symbol '{}' at position {}", c, i));
         }
     }
 
@@ -61,20 +62,21 @@ WideInt::WideInt(std::string num) {
     // TODO: simplify and describe this
     base d = 1;
     if (point < start) {
+        // 0.00...00123
         exp = -((end - point) + (PART_SIZE - 1)) / PART_SIZE;
     } else if (point > end) {
+        // 12300...00.0
         exp = (point - end - 1) / PART_SIZE;
         end += (point - end - 1) % PART_SIZE;
-    } else  {
+    } else {
         exp = -((end - point) + (PART_SIZE - 1)) / PART_SIZE;
-        i = (end - point) % PART_SIZE;
         d = pow(10, PART_SIZE - (end - point) % PART_SIZE);
     }
 
-    // FIXME: `end - start + 1` can count point symbol
+    // FIXME: `end - start + 1` may count point symbol
     // FIXME: `max(..., 2)` handles exceptional case when number is small enough to be contained
-    //         in one part but shouldn't due to floating point (like 1.25)
-    parts = std::vector<base>( std::max(((end - start + 1) + (PART_SIZE - 1)) / PART_SIZE, 2), 0 );
+    //         in one part but shouldn't due to floating point (like `1.25`)
+    parts = std::vector<base>(std::max(((end - start + 1) + (PART_SIZE - 1)) / PART_SIZE, 2), 0);
 
     for (int i = end; i >= start; --i) {
         // TODO: Workaround to remove extra check from loop
@@ -89,36 +91,37 @@ WideInt::WideInt(std::string num) {
 }
 
 void WideInt::print() {
+    using namespace std;
+
     if (sign) {
-        std::cout << '-';
+        cout << '-';
     }
 
     int point = parts.size() + exp;
 
     if (point <= 0) {
-        std::cout << "0.";
-
+        cout << "0.";
         for (int i = 0; i < point; ++i) {
-            std::cout << std::setfill('0') << std::setw(9) << 0;
+            cout << setfill('0') << setw(9) << 0;
         }
     }
 
     for (int i = 1; i <= parts.size(); ++i) {
         if (i != 1) {
-            std::cout << std::setfill('0') << std::setw(9);
+            cout << setfill('0') << setw(9);
         }
-        std::cout << parts[parts.size() - i];
+        cout << parts[parts.size() - i];
 
-        if (point == i)  {
-            std::cout << '.';
+        if (point == i) {
+            cout << '.';
         }
     }
 
     for (int i = 0; i < exp; ++i) {
-        std::cout << std::setfill('0') << std::setw(9) << 0;
+        cout << setfill('0') << setw(9) << 0;
     }
 
-    std::cout << std::endl;
+    cout << endl;
 };
 
 int8_t WideInt::compare(const WideInt &other) {
@@ -135,14 +138,14 @@ WideInt WideInt::operator-(const WideInt &other) const {
 
 }
 
-// TODO: Check is it the right way to do
 WideInt WideInt::operator-() {
-    sign = !sign;
-    return *this;
+    WideInt w(*this);
+    w.sign = !w.sign;
+    return w;
 }
 
 int main(void) {
 //    WideInt a = -0.10234235124000000000_w;
-    WideInt a = 1.25_w;
+    WideInt a = -12_w;
     a.print();
 }
