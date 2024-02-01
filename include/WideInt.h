497d364b155b34d6affd531926cb88a372cd9f8d
reymagos <andalepotapov@edu.hse.ru>
2024-02-02 00:51:27 +0300

    String conversion and print function

--- include/WideInt.h
+++ include/WideInt.h
@@ -2,26 +2,28 @@
 #include <string>
 #include <vector>
 
+using base = uint32_t;
 #define PART_SIZE 9
 #define PART_MAX 1000000000
 
 class WideInt {
     int8_t sign;
-    uint32_t length;
-    uint32_t point;
-    std::vector<uint32_t> parts;
+    int32_t exp;
+    std::vector<base> parts;
 
     int8_t compare(const WideInt&);
 
 public:
-    WideInt();
+    WideInt() = default;
     WideInt(std::string);
     bool operator<(const WideInt&);
     WideInt operator+(const WideInt&) const;
     WideInt operator-(const WideInt&) const;
+    WideInt operator-();
+    void print();
 };
 
-// constexpr?
+// TODO: constexpr?
 WideInt operator""_w(const char *str) {
     return WideInt(str);
 }
